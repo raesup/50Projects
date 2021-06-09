a5088c6678163d7ed4b9bb8edc6e2f594b7d6437
unknown <raesupkim@gmail.com>
2021-06-09 13:01:05 -0600

    Split Landing Page

---
+++ Split Landing Page/script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector('.container');
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'));
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
+
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
+
+/* bellow is not working, why?
+left.addEventListener('mouseenter', hoverLeft());
+
+const hoverLeft = () => {
+    container.classList.add('hover-left');
+}
+*/
