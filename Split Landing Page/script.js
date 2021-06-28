b739d5a3f79836096779573d7f03ffeeccfcdb31
unknown <raesupkim@gmail.com>
2021-06-28 15:39:32 -0600

    Drag N Drop

--- Split Landing Page/script.js
+++ Split Landing Page/script.js
@@ -1,12 +1,20 @@
-const container = document.querySelector('.container');
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
+const container = document.querySelector(".container");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
 
-left.addEventListener('mouseenter', () => container.classList.add('hover-left'));
-left.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
+left.addEventListener("mouseenter", () =>
+  container.classList.add("hover-left")
+);
+left.addEventListener("mouseleave", () =>
+  container.classList.remove("hover-left")
+);
 
-right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
-right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
+right.addEventListener("mouseenter", () =>
+  container.classList.add("hover-right")
+);
+right.addEventListener("mouseleave", () =>
+  container.classList.remove("hover-right")
+);
 
 /* bellow is not working, why?
 left.addEventListener('mouseenter', hoverLeft());
