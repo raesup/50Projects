82426274bb8c705a77371f2eb9f25584d24999ae
unknown <raesupkim@gmail.com>
2021-07-04 23:01:26 -0600

    Toast Notification

--- Toast Notification/script.js
+++ Toast Notification/script.js
@@ -1,1 +1,34 @@
+const button = document.getElementById("button");
+const toasts = document.getElementById("toasts");
 
+const messages = [
+  "Message One",
+  "Message Two",
+  "Message Three",
+  "Message Four",
+];
+
+const types = ["info", "error", "success"];
+
+button.addEventListener("click", () => {
+  createNotification();
+});
+
+function createNotification() {
+  const toast = document.createElement("div");
+  toast.classList.add("toast");
+  const message = randomToast();
+  const type = randomType();
+  toast.textContent = messages[message];
+  toast.classList.add(type);
+  toasts.appendChild(toast);
+  setTimeout(() => toast.remove(), 2000);
+}
+
+function randomToast() {
+  return Math.floor(Math.random() * messages.length);
+}
+
+function randomType() {
+  return types[Math.floor(Math.random() * types.length)];
+}
