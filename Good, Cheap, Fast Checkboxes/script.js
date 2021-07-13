f13411b2e1053e771fadb6c631f0ea2a09ff7639
unknown <raesupkim@gmail.com>
2021-07-13 15:53:59 -0600

    Good Cheap Fast Checkboxes

--- Good, Cheap, Fast Checkboxes/script.js
+++ Good, Cheap, Fast Checkboxes/script.js
@@ -1,1 +1,23 @@
+const goodEl = document.getElementById("good");
+const cheapEl = document.getElementById("cheap");
+const fastEl = document.getElementById("fast");
+const toggles = document.querySelectorAll(".toggle");
 
+toggles.forEach((toggle) => {
+  toggle.addEventListener("change", (e) => {
+    if (goodEl.checked && cheapEl.checked && fastEl.checked) {
+      console.log(e, e.target);
+      if (e.target === goodEl) {
+        fastEl.checked = false;
+      }
+
+      if (e.target === cheapEl) {
+        goodEl.checked = false;
+      }
+
+      if (e.target === fastEl) {
+        cheapEl.checked = false;
+      }
+    }
+  });
+});
