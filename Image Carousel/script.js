a1b11397e5916b18f6985723189050e3dc2c4c49
unknown <raesupkim@gmail.com>
2021-09-16 21:55:58 -0600

    Image Carousel final

--- Image Carousel/script.js
+++ Image Carousel/script.js
@@ -1,1 +1,42 @@
+const imgs = document.querySelector("#imgs");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
 
+const img = document.querySelectorAll("#imgs img");
+
+let i = 0;
+
+let interval = setInterval(run, 2000);
+
+function run() {
+  i++;
+
+  changeImage();
+}
+
+function changeImage() {
+  if (i > img.length - 1) {
+    i = 0;
+  } else if (i < 0) {
+    i = img.length - 1;
+  }
+
+  imgs.style.transform = `translateX(${-i * 500}px)`;
+}
+
+function resetInterval() {
+  clearInterval(interval);
+  interval = setInterval(run, 2000);
+}
+
+rightBtn.addEventListener("click", () => {
+  i++;
+  changeImage();
+  resetInterval();
+});
+
+leftBtn.addEventListener("click", () => {
+  i--;
+  changeImage();
+  resetInterval();
+});
